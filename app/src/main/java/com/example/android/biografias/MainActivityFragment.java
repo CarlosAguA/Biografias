bab5bfc71e932136dc2a06fa399747a1ba45d4c6
Carlos Aguilera <ceaguileraa@tecmor.mx>
2017-06-15 14:24:25 -0500

    Modif sencilla

--- app/src/main/java/com/example/android/biografias/MainActivityFragment.java
+++ app/src/main/java/com/example/android/biografias/MainActivityFragment.java
@@ -1,11 +1,14 @@
 package com.example.android.biografias;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.android.biografias.data.Heroes;
 import com.example.android.biografias.data.HeroesAdapter;
@@ -35,14 +38,43 @@ public class MainActivityFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-
         heroeAdapter = new HeroesAdapter(getActivity(), Arrays.asList(heroesArray)) ;
 
         // 3. Encontrar listView con su id y asignar adaptador al listView
-        ListView listView = (ListView) rootView.findViewById(R.id.list);
-        listView.setAdapter(heroeAdapter);
+        ListView lv = (ListView) rootView.findViewById(R.id.list);
+        lv.setAdapter(heroeAdapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                Heroes heroeSeleccionado =  heroeAdapter.getItem(position);
+
+                String name = heroeSeleccionado.getNombre();
+
+                Toast.makeText(getActivity(),name ,
+                        Toast.LENGTH_SHORT).show();
+
+                showDescripcion(position);
+
+            }
+        });
 
         return rootView ;
     }
+
+    private void showDescripcion(int position) {
+
+        // Create an intent for starting the DetailsActivity
+        Intent intent = new Intent();
+
+        // explicitly set the activity context and class
+        // associated with the intent (context, class)
+        intent.setClass(getActivity(), DetailsActivity.class);
+
+        startActivity(intent);
+
+    }
+
 
 }
