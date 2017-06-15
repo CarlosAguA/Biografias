6a89caedcfb5995305dca745a962dd798a9f1d9e
Carlos Aguilera <ceaguileraa@tecmor.mx>
2017-06-15 16:50:55 -0500

    Collapsing layout to third screen without fragment and most simple set up

--- app/src/main/java/com/example/android/biografias/DetailsActivity.java
+++ app/src/main/java/com/example/android/biografias/DetailsActivity.java
@@ -1,13 +1,65 @@
 package com.example.android.biografias;
 
+import android.graphics.Rect;
+import android.support.design.widget.AppBarLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    private Rect mStart = new Rect();
+
+    private Rect mTarget = new Rect();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("Abby");
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        final View target = findViewById(R.id.circle_collapsed_target);
+
+        final CircleImageView circleImageView = (CircleImageView) findViewById(R.id.circle_image_view);
+
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.app_bar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_sign_out: {
+                // do your sign-out stuff
+                break;
+            }
+            // case blocks for other MenuItems (if any)
+        }
+        // case blocks for other MenuItems (if any)
+        return super.onOptionsItemSelected(item);
     }
 }
